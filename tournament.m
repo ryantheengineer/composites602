ecaec86f2e4fd999ae3bb2ed4cc3ed8b8e4f3a86
ryantheengineer <ryan.larson.wa@gmail.com>
2019-12-04 00:06:58 -0700

    Added getFitness.m, maximin.m, and tournament.m

---
+++ tournament.m
@@ -0,0 +1,12 @@
+function [winner] = tournament(parents,size)
+% size = number of parents to compete with each other
+numParents = length(parents);
+for j = 1:numParents
+    for i = 1:size
+        designNum(i) = 1 + floor(rand*numParents);
+        Score(i) = maximin(parents(designNum(i)),parents);
+    end
+    [~, index] = min(Score);
+    winner(j) = parents(designNum(index));
+end
+
